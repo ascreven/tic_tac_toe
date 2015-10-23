813e48668d0c28b528df41dd0587a2ce69e057b4
Alexandra <akscreven@gmail.com>
2015-10-23 10:14:26 -0400

    reset button added

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ $(document).ready(function () {
   $('p').on('click',function (){
     $(this).css('background','blue');
   });
+  $('button').on('click',function (){
+    $('p').css('background','white');
+  });
 });
