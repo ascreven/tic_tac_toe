5bd8445c1571c3c25d8fab5c7edc175075210703
Alexandra <akscreven@gmail.com>
2015-10-23 12:20:49 -0400

    players take turns and cannot pick a cell that's already in play

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 $(document).ready(function () {
+  //header showing whose turn it is
+  var player = 1;
+  var turn = $('h2').html("It's Player " + player + "'s Turn");
 
+//players take turns and cannot click on a cell that already has a value
   $('p').on('click',function (){
-    $(this).css('background','blue');
+    var cellImage = $(this).css('background-image');
+    if (player == 1 & cellImage == 'none') {
+      // $(this).css('background-image','no-repeat');
+      $(this).css('background-image','url(O.png)');
+      player = 2;
+      var turn = $('h2').html("It's Player " + player + "'s Turn");
+    } else if (cellImage == 'none'){
+      $(this).css('background-image','url(X.png)');
+      player = 1;
+      var turn = $('h2').html("It's Player " + player + "'s Turn");
+    }
   });
+
+  //if reset is hit all the cells are cleared
   $('button').on('click',function (){
-    $('p').css('background','white');
+    $('p').css('background-image','none');
   });
 });
